7e85fe6a729778211a6d9e7dd9e8ad22418a3eb6
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2025-05-13 09:24:11 +0100

    Update __init__.py

--- h2xh2/algorithm/__init__.py
+++ h2xh2/algorithm/__init__.py
@@ -5,7 +5,7 @@ from ._circuits import (
 from ._utils import (
     get_mu_and_sigma,
 )
-from ._bayesian_qpe import bayesian_update
+from ._bayesian_qpe import bayesian_update, get_ms
 
 __all__ = [
     "get_qpe_func",
